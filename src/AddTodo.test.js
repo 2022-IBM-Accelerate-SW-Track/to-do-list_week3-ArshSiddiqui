4d1a5812ee067b087b2f4e4708dbb319cec1dad4
arshsiddiqui <arshsiddiqui@gmail.com>
2022-06-22 09:27:08 -0400

    Changes to the files as requested from the GitHub page

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -21,23 +21,99 @@ afterEach(() => {
 
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  const text = 'How now brown cow';
+  const dueDate = "12/25/2022";
+  fireEvent.change(inputTask, {target: {value: text}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(button);
+
+  fireEvent.change(inputTask, {target: {value: text}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(button);
+
+  const check = screen.getAllByText(/How now brown cow/i);
+  expect(check.length).toBe(1);
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  // No task name, but due date present
+  const dueDate = "12/25/2022";
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(button);
+  const check = screen.getAllByRole('link');
+  expect(check.length).toBe(2);
+
+  // No task name, and no due date
+  fireEvent.click(button);
+  const secondCheck = screen.getAllByRole('link');
+  expect(secondCheck.length).toBe(2);
  });
 
  test('test that App component doesn\'t add a task without due date', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  // No due date, but task name present
+  const text = 'How now brown cow';
+  fireEvent.change(inputTask, {target: {value: text}});
+  fireEvent.click(button);
+  const check = screen.getAllByRole('link');
+  expect(check.length).toBe(2);
+
+  // No task name, and no due date
+  fireEvent.click(button);
+  const secondCheck = screen.getAllByRole('link');
+  expect(secondCheck.length).toBe(2);
  });
 
 
 
  test('test that App component can be deleted thru checkbox', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  const text = 'How now brown cow';
+  const dueDate = "12/25/2022";
+  fireEvent.change(inputTask, {target: {value: text}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(button);
+
+  const check = screen.getAllByRole('link');
+  expect(check.length).toBe(3);
+
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+
+  const secondCheck = screen.getAllByRole('link');
+  expect(secondCheck.length).toBe(2);
  });
 
 
  test('test that App component renders different colors for past due events', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const button = screen.getByRole('button', {name: /Add/i});
+
+  const text = 'How now brown cow';
+  const dueDate = "12/25/2021";
+  fireEvent.change(inputTask, {target: {value: text}});
+  fireEvent.change(inputDate, {target: {value: dueDate}});
+  fireEvent.click(button);
+
+  const checkColor = screen.getByTestId(/How now brown cow/i).style.background;
+  expect(checkColor).toBe("red");
  });
